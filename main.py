be2e94da5995833fecf9a87e75a3822fa655ed1e
yu <2454789598@qq.com>
2025-05-10 17:15:31 +0800

    版本2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-#1111111111111111
+#222222222222222
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
